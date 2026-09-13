ef080f5067e807d12ee69f8cd708efe1246cb7a7
Abdul Alim <abdulalimad17@gmail.com>
2026-09-13 21:21:44 +0600

    Initial Card Structure

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -4,7 +4,7 @@ import YourStack from "./YourStack"
 const Section2 = () => {
   return (
     <div className='min-h-screen bg-amber-50'>
-      <div className=" flex-col px-20 py-25">
+      <div className="flex-col px-20 py-10 w-fit mb-7">
         <h1 className="font-bold text-4xl mb-2">Explore the <span className="bg-linear-to-r from-fuchsia-400 to-fuchsia-700 font- bg-clip-text text-transparent">Technologies</span></h1>
         <p className="text-gray-500">Pick one technology per category to build your ideal stack</p>
       </div>
--- src/components/section2/YourStack.jsx
+++ src/components/section2/YourStack.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const YourStack = () => {
   return (
-    <div>
-      
+    <div className='h-83  px-5 py-5 w-75 mx-10 my-4 border tracking-tighter border-gray-300 rounded-xl'>
+
+      <div className="flex mb-7 items-center justify-between">
+        <img className='h-7' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35wn1WS6yA_lP2clChNCuxdD8KK3DcP-vC-bSJlxjYg&s" alt="" />
+        <h4 className='bg-blue-50 border border-blue-300 rounded-xl w-20 text-center font-bold text-blue-400'>Popular</h4>
+      </div>
+
+      <div className="center">
+        <h2 className='font-bold text-2xl mb-2'>React</h2>
+        <p className='text-gray-600 mb-4'>A declarative, component-based JavaScript library for building modern user interfaces.</p>
+      </div>
+
+      <div className="flex border-t border-gray-200 mb-8 pt-5 items-center justify-between ">
+        <h4 className='bg-gray-100 w-20 text-center rounded-md'>Frontend</h4>
+        <p className='text-gray-500 tracking-tighter'>Beginner-Friendly</p>
+        <h4 className='font-semibold'><span className=" text-yellow-500">★</span> 4.9</h4>
+      </div>
+
+      <div className='flex justify-center items-center '>
+              <button className='bg-black rounded-2xl text-white w-40 h-8'>Add to Stack</button>
+
+      </div>
+
     </div>
   )
 }
